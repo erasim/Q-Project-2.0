eb5b264047e202ad6176ee42785b1e2a952718f5
Asim Hasnain <124768339+asim-quencil@users.noreply.github.com>
2023-03-03 15:02:56 +0530

    page2

--- src/component/home/Mainbar.jsx
+++ src/component/home/Mainbar.jsx
@@ -25,7 +25,8 @@ import oktic from "../../Images/oktic.png";
 
 const Mainbar = () => {
 	const [Active, setActive] = useState(false);
-
+	const BLOCK = { display: 'block' }
+	const NONE= { display: 'none' }
 	return (
 		<div className='Mainbar-main'>
 			<div className='sub-navbar'>
@@ -53,7 +54,15 @@ const Mainbar = () => {
 						Assembely
 					</NavLink>
 				</div>
-
+				<div class="dropdown-navbar">
+				
+  <button className="dropbtn" onClick={}>Dropdown</button>
+  <div className="dropdown-content">
+    <a href="#">Link 1</a>
+    <a href="#">Link 2</a>
+    <a href="#">Link 3</a>
+  </div>
+</div>
 				
 
 
@@ -72,6 +81,35 @@ const Mainbar = () => {
 						Add Products
 					</button>
 				</div>
+				<div className='Mainbar-main-sub-navbar-button2'>
+					<button>
+						<svg
+							xmlns='http://www.w3.org/2000/svg'
+							width='16'
+							height='16'
+							fill='currentColor'
+							class='bi bi-plus-circle'
+							viewBox='0 0 16 16'>
+							<path d='M8 15A7 7 0 1 1 8 1a7 7 0 0 1 0 14zm0 1A8 8 0 1 0 8 0a8 8 0 0 0 0 16z' />
+							<path d='M8 4a.5.5 0 0 1 .5.5v3h3a.5.5 0 0 1 0 1h-3v3a.5.5 0 0 1-1 0v-3h-3a.5.5 0 0 1 0-1h3v-3A.5.5 0 0 1 8 4z' />
+						</svg>
+						Add
+					</button>
+				</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 			</div>
 			<div className='sub-navbar-filterbar-main'>
 				<div className='sub-navbar-filterbar-left'>
